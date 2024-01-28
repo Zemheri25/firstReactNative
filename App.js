db23d4ec26609adf0b769e990def34ea10302b81
Ömer Zemheri <omer.zemheri@fonetyazilim.com>
2024-01-28 19:33:59 +0300

    Banner added

--- App.js
+++ App.js
@@ -1,6 +1,15 @@
-import { StyleSheet, View, FlatList, Text, Image } from "react-native";
+import {
+  StyleSheet,
+  View,
+  FlatList,
+  Text,
+  Image,
+  ScrollView,
+  Dimensions,
+} from "react-native";
 import news_data from "./news_data.json";
 import { News } from "./components/News";
+import newsBanner from "./news_banner.json";
 
 export default function App() {
   const renderNews = ({ item }) => {
@@ -10,7 +19,22 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.mainHeader}>News</Text>
-      <FlatList data={news_data} renderItem={renderNews} />
+
+      <FlatList
+        ListHeaderComponent={() => (
+          <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+            {newsBanner.map((item) => (
+              <Image
+                key={item.id}
+                style={styles.image}
+                source={{ uri: item.imageUrl }}
+              />
+            ))}
+          </ScrollView>
+        )}
+        data={news_data}
+        renderItem={renderNews}
+      />
     </View>
   );
 }
@@ -25,5 +49,12 @@ const styles = StyleSheet.create({
   mainHeader: {
     fontWeight: "900",
     fontSize: 30,
+  },
+
+  image: {
+    height: Dimensions.get("window").height / 5,
+    width: Dimensions.get("window").width / 2,
+    borderRadius: 20,
+    marginLeft: 10,
   },
 });
